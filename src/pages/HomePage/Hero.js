6f6def23e5048d2075ab6e713c1f5f24326d258e
mr-new-0509 <mr.new0509@gmail.com>
2022-01-07 20:43:55 +0900

    hero

--- src/pages/HomePage/Hero.js
+++ src/pages/HomePage/Hero.js
@@ -15,18 +15,30 @@ export default function Hero() {
         <div className="flex md:flex-row lg:flex-row mx-10 md:mx-32 lg:mx-32 lg:mt-24">
           <div className="flex flex-col lg:w-3/5 items-stretch pt-0 md:pt-10 lg:pt-10 pb-16 md:pb-32 lg:pb-32">
             <MotionInView variants={varFadeInLeft}>
-              <h1 className="font-medium text-transparent bg-clip-text header-gradent mt-5 md:mt-0 mr-0 lg:mr-44 lg:mt-0 text-2xl md:text-4xl lg:text-[62px] ">
-                Reimagining NFT management through the raw power of the blockchain
+              <h1 className="font-bold text-transparent bg-clip-text header-gradent mt-5 md:mt-0 mr-0 lg:mt-0 text-2xl md:text-4xl lg:text-[62px] ">
+                Reimagining NFT management through the raw power <br /> of the blockchain
               </h1>
             </MotionInView>
 
             {/* <div className="border-t border-l border-white mt-14 inline-block"> */}
             <MotionInView variants={varFadeInUp}>
-              <a href="/assets/pdf/VAULT3D-Whitepaper.pdf" target="_blank">
-                <button className="px-6 py-2 mt-14 text-3xl bg-white bg-steel-button bg-cover bg-center text-black font-bold uppercase focus:shadow-outline hover:bg-indigo-600 hover:text-themePrimaryDark-100">
-                  Start Collect
-                </button>
-              </a>
+              <div className="relative">
+                <div className="absolute z-20 left-4 top-4">
+                  <a href="/assets/pdf/VAULT3D-Whitepaper.pdf" target="_blank">
+                    <button className="px-12 py-4 mt-14 text-3xl bg-white bg-steel-button bg-cover bg-center text-black font-bold uppercase focus:shadow-outline hover:bg-indigo-600 hover:text-themePrimaryDark-100">
+                      Start Collect
+                    </button>
+                  </a>
+                </div>
+                <div className="absolute z-10">
+                  <a href="/assets/pdf/VAULT3D-Whitepaper.pdf" target="_blank">
+                    <button className="px-12 py-4 mt-14 text-3xl bg-transparent text-transparent font-bold uppercase bottom-button-gradient">
+                      Start Collect
+                    </button>
+                  </a>
+                </div>
+              </div>
+
             </MotionInView>
             {/* </div> */}
           </div>
