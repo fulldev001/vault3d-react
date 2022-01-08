22155b0a1bf520a01f635ed286eb818366dc8f98
mr-new-0509 <mr.new0509@gmail.com>
2022-01-08 23:56:49 +0900

    team meber section removed

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Footer from '../../components/Footer';
 import TopHeader from "../../components/TopHeader";
-import AmbassadorsBlock from './AmbassadorsBlock';
+// import AmbassadorsBlock from './AmbassadorsBlock';
 import FaqBlock from './FaqBlock';
 import Hero from './Hero';
 import HowToBuyBlock from './HowToBuyBlock';
-import OurCoreTeamBlock from './OurCoreTeamBlock';
+// import OurCoreTeamBlock from './OurCoreTeamBlock';
 import PanelBlock from './PanelBlock';
 import Roadmap from './Roadmap';
 import StatementBlock from './StatementBlock';
@@ -21,8 +21,8 @@ export default function HomePage() {
       <TokenomicsBlock />
       <Roadmap />
       <HowToBuyBlock />
-      <OurCoreTeamBlock />
-      <AmbassadorsBlock />
+      {/* <OurCoreTeamBlock /> */}
+      {/* <AmbassadorsBlock /> */}
       <FaqBlock />
       <Footer />
     </div>
