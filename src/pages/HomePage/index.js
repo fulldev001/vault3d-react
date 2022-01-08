1669329d44150929d28f6f5f4fe814cd1c4da471
mr-new-0509 <mr.new0509@gmail.com>
2022-01-08 14:57:47 +0900

    TokenomicsBlock - mobile

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -18,7 +18,7 @@ export default function HomePage() {
       <Hero />
       <PanelBlock />
       <StatementBlock />
-      {/* <TokenomicsBlock /> */}
+      <TokenomicsBlock />
       {/* <Roadmap /> */}
       {/* <HowToBuyBlock /> */}
       {/* <OurCoreTeamBlock /> */}
