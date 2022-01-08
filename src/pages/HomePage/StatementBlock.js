81e1778feb33460416208f72ed1a6efe2b51f3b6
mr-new-0509 <mr.new0509@gmail.com>
2022-01-08 14:43:51 +0900

    Statement-mobile

--- src/pages/HomePage/StatementBlock.js
+++ src/pages/HomePage/StatementBlock.js
@@ -17,7 +17,8 @@ export default function StatementBlock() {
         <div className="container mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-0">
             <div className="">
-              <MotionInView variants={varFadeInLeft}>
+              {/* Desktop */}
+              <MotionInView variants={varFadeInLeft} className="hidden lg:block md:hidden">
                 <div className="text-outline-left relative" title="SHOW IT">
                   <h1 className="relative lg:text-[60px] md:text-[46px] font-bold text-transparent bg-clip-text inline-block bg-steel-button bg-cover bg-center uppercase z-20">
                     Show IT
@@ -29,13 +30,41 @@ export default function StatementBlock() {
                   </h1>
                 </div>
               </MotionInView>
+
+              {/* Tablet */}
+              <MotionInView variants={varFadeInLeft} className="hidden md:block lg:hidden">
+                <div className="text-outline-left-tablet relative" title="SHOW IT">
+                  <h1 className="relative lg:text-[60px] md:text-[46px] font-bold text-transparent bg-clip-text inline-block bg-steel-button bg-cover bg-center uppercase z-20">
+                    Show IT
+                  </h1>
+                </div>
+                <div className="text-outline-left-tablet relative" title="TO THE WORLD!">
+                  <h1 className="relative md:text-[46px] lg:text-[60px] font-bold text-transparent bg-clip-text inline-block bg-steel-button bg-cover bg-center uppercase z-20">
+                    To The World!
+                  </h1>
+                </div>
+              </MotionInView>
+
+              {/* Mobile */}
+              <MotionInView variants={varFadeInLeft} className="block lg:hidden md:hidden">
+                <div className="text-outline-left-mobile relative" title="SHOW IT">
+                  <h1 className="relative text-[28px] lg:text-[60px] md:text-[46px] font-bold text-transparent bg-clip-text inline-block bg-steel-button bg-cover bg-center uppercase z-20">
+                    Show IT
+                  </h1>
+                </div>
+                <div className="text-outline-left-mobile relative" title="TO THE WORLD!">
+                  <h1 className="relative text-[28px] md:text-[46px] lg:text-[60px] font-bold text-transparent bg-clip-text inline-block bg-steel-button bg-cover bg-center uppercase z-20">
+                    To The World!
+                  </h1>
+                </div>
+              </MotionInView>
               <MotionInView variants={varFadeInRight}>
                 <p className="lg:text-2xl md:text-lg md:w-full lg:w-4/5 text-left mt-10 text-white">
                   Your originality is the strength of your NFT
                 </p>
               </MotionInView>
               {/* White sphere for desktop */}
-              <div className="mt-20 md:hidden lg:block">
+              <div className="mt-20 hidden md:hidden lg:block">
                 <MotionInView variants={varFadeInUp}>
                   <img
                     src="/assets/images/badge-small.png"
@@ -46,10 +75,10 @@ export default function StatementBlock() {
                 </MotionInView>
               </div>
             </div>
-            <div className="md:flex md:justify-between md:items-baseline text-center mt-10 md:mt-0 lg:mt-32">
+            <div className="flex justify-between md:flex md:justify-between md:items-baseline items-baseline text-center mt-10 md:mt-0 lg:mt-32">
 
               {/* White sphere for tablet */}
-              <div className="mt-20 lg:hidden md:block md:w-1/5">
+              <div className="mt-20 hidden lg:hidden md:block md:w-1/5">
                 <MotionInView variants={varFadeInUp}>
                   <img
                     src="/assets/images/badge-small.png"
@@ -60,11 +89,22 @@ export default function StatementBlock() {
                 </MotionInView>
               </div>
 
+              {/* White sphere for mobile */}
+              <div className="mt-20 block lg:hidden md:hidden md:w-1/5">
+                <MotionInView variants={varFadeInUp}>
+                  <img
+                    src="/assets/images/badge-small.png"
+                    alt=""
+                    className="w-[150px]"
+                  />
+                </MotionInView>
+              </div>
+
               {/* Space for tablet */}
-              <div className="md:w-1/5 lg:hidden"></div>
-              <div className="relative md:w-3/5 lg:w-full">
+              <div className="md:w-1/5 hidden lg:hidden"></div>
+              <div className="relative w-full md:w-3/5 lg:w-full">
                 {/* Curve grid for desktop */}
-                <div className="relative w-500 h-500 overflow-hidden md:hidden lg:block">
+                <div className="relative w-500 h-500 overflow-hidden hidden md:hidden lg:block">
                   <MotionInView variants={varFadeInUp}>
                     <img
                       src="/assets/images/gridbgright.png"
@@ -75,7 +115,7 @@ export default function StatementBlock() {
                 </div>
 
                 {/* Curve grid for tablet */}
-                <div className="lg:hidden">
+                <div className="hidden lg:hidden md:block">
                   <MotionInView variants={varFadeInUp}>
                     <img
                       src="/assets/images/green_curve_grid.png"
@@ -85,7 +125,18 @@ export default function StatementBlock() {
                   </MotionInView>
                 </div>
 
-                <div className="absolute md:py-0 lg:py-2.5 md:px-0 lg:px-20 top-16 md:top-36 lg:top-56 inset-x-0 text-white lg:text-2xl md:text-lg text-left">
+                {/* Curve grid for mobile */}
+                <div className="block md:hidden lg:hidden">
+                  <MotionInView variants={varFadeInUp}>
+                    <img
+                      src="/assets/images/green_curve_grid.png"
+                      alt=""
+                      className="h-[200px]"
+                    />
+                  </MotionInView>
+                </div>
+
+                <div className="absolute md:py-0 lg:py-2.5 md:px-0 lg:px-20 top-10 md:top-36 lg:top-56 inset-x-0 text-white lg:text-2xl md:text-lg lg:text-left md:text-left text-right">
                   <MotionInView variants={varFadeInDown}>
                     VOULT3D will make it easier than ever to create, upload,
                     price, and show off your NFTs!
--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -17,7 +17,7 @@ export default function HomePage() {
       <TopHeader />
       <Hero />
       <PanelBlock />
-      {/* <StatementBlock /> */}
+      <StatementBlock />
       {/* <TokenomicsBlock /> */}
       {/* <Roadmap /> */}
       {/* <HowToBuyBlock /> */}
